2727519b532f60d5e7502f29d4af5dbd7cabfc9e
Khaled Elorbany <khaled.wagdy554@gmail.com>
2024-07-31 17:18:46 +0300

    resolver and schema

--- resolvers.js
+++ resolvers.js
@@ -2,25 +2,41 @@ import db from './_db.js';
 
 export const resolvers = {
   Query: {
-    review(_, args) {
-      return db.reviews.find((review) => review.id === args.id);
-    },
-    reviews() {
-      return db.reviews;
-    },
-
-    game(_, args) {
-      return db.games.find((game) => game.id === args.id);
-    },
     games() {
-      return db.games;
+      return db.games
     },
-    
-    author(_, args) {
-      return db.authors.find((author) => author.id === args.id);
+    game(_, args) {
+      return db.games.find((game) => game.id === args.id)
     },
     authors() {
-      return db.authors;
+      return db.authors
     },
-  }
+    author(_, args) {
+      return db.authors.find((author) => author.id === args.id)
+    },
+    reviews() {
+      return db.reviews
+    },
+    review(_, args) {
+      return db.reviews.find((review) => review.id === args.id)
+    }
+  },
+  Game: {
+    reviews(parent) {
+      return db.reviews.filter((r) => r.game_id === parent.id)
+    }
+  },
+  Review: {
+    author(parent) {
+      return db.authors.find((a) => a.id === parent.author_id)
+    },
+    game(parent) {
+      return db.games.find((g) => g.id === parent.game_id)
+    }
+  },
+  Author: {
+    reviews(parent) {
+      return db.reviews.filter((r) => r.author_id === parent.id)
+    }
+  },
 }
